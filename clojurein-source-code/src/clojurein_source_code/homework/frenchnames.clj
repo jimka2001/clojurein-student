8f95bfeea7ce88a46f24a66de5ac5e9ca8493ab7
Jim Newton <jnewton@lrde.epita.fr>
2023-08-17 15:29:26 +0200

    synchronized

---
+++ clojurein-source-code/src/clojurein_source_code/homework/frenchnames.clj
@@ -0,0 +1,120 @@
+(ns clojurein-source-code.homework.frenchnames
+  (:require [clojure.string :refer [lower-case split index-of]]
+            [clojurein-source-code.common.util :refer [fff]]
+            [clojure.java.io :as io]
+            [clojure.pprint :refer [cl-format]]))
+
+(defn elide-string
+  "replace characters having diacritical marks with simple characters
+   E.g.,  'æ' => \"ae\"
+          'â' | 'ä' | 'à' => \"a\"
+          'è' | 'é' | 'ë' | 'ê' => \"e\"
+   etc for every special character in the file
+   "
+  [name]
+  (reduce str ""
+          (mapcat (fn [c]
+                    (let [new-string
+                          (if (or (<= (int \a) (int c) (int \z))
+                                  (<= (int \A) (int c) (int \Z))
+                                  (= c \-)
+                                  (= c \'))
+                            (str c)
+                            (case c
+                              (\æ) "ae"
+                              (\â  \ä \à) "a"
+                              (\ç) "c"
+                              ;; CHALLENGE: student must complete the implementation.
+                              (throw (ex-info "Missing one or more expressions, not yet implemented" {}))
+                              ))]
+                      (if new-string
+                        new-string
+                        (throw (ex-info (cl-format false "unknown character [~A] [~A]" c (int c))
+                                        {:int (int c)
+                                         :char c})))))
+                  name)))
+
+(defn baby-names-per-year
+  ;; this function returns a Map which maps year to count, where count is
+  ;; the number of babies named nameTarget in that year.  E.g., if
+  ;; fred = (fbabyNamesPerYear "fred" "M"), then (fred 2012)==100 means
+  ;; there were 100 boy babies born named fred in 2012."
+  ([name-target gender-target]
+   (throw (ex-info "Missing single expression, not yet implemented" {}))
+   )
+
+  ;; This function returns a hash-map which associates
+  ;;   to each year (key) the number of babies born (value) in that
+  ;;   year of the given gender and the given name.
+  ;;
+  ;; Some French names can be spelled using different characters.
+  ;; E.g., jerome vs jérôme vs jérome
+  ;; This function has a Boolean flag indicating whether to elide different
+  ;; spellings into one.  if elide=false, then "jerome" and "jérôme" are
+  ;; considered different names, thus the Map returned might have keys
+  ;; "jerome" and "jérôme".  If elide=true, then "jerome" and "jérôme"
+  ;; are considered the same, in which case the Map returned will have
+  ;; key "jerome" but not "jérôme" ... babies named "jérôme" are considered
+  ;; to have the name "jerome" when elide=true.
+  ;;
+  ;; If elide is true, then the Map returned will not contain key "jérôme"
+  ;;    but it may contain "jerome", and the total count for each year
+  ;;    will contain the total of the babies named "jérôme" + babies named
+  ;;    "jérome" + babies named "jerome" etc.
+  ;; If elide is false, then the Map may contain keys "jérôme", "jérome",
+  ;;    "jerome" etc.
+  ([name-target gender-target elide]
+   (assert (= name-target (lower-case name-target))
+           (cl-format false "baby-names-per-year must be called with lower case name, not [~A]" name-target))
+   (let [s (io/resource "France-baby-names/nat2020.csv")]
+     (with-open [r (io/reader s)]
+       ;; remember to drop the first line of file: sexe;preusuel;annais;nombre
+       ;; skip lines where name is _PRENOMS_RARES
+       ;; skip lines where the year is XXXX
+       ;; names are in caps (e.g., FABIENNE) but nameTarget will be given as lower case (e.g., fabienne)
+       
+       ;; CHALLENGE: student must complete the implementation.
+       (throw (ex-info "Missing one or more expressions, not yet implemented" {}))
+       ))))
+
+
+(defn count-names-in-year-range
+  "count the number of babies born between year-min and year-max (including both year-min and year-max)
+   of the given gender and name.
+  Return an integer.
+  The parameter elide, indicates (as above) whether to consider names
+    such as \"jerome\" and \"jérôme\" as the same or different.
+  If elide is true, then \"jerome\" and \"jérôme\" are considered the same.
+  If elide is false, then \"jerome\" and \"jérôme\" are considered to be different names.
+  "
+  [name gender year-min year-max elide]
+  (let [names (baby-names-per-year name gender elide)]
+    ;; CHALLENGE: student must complete the implementation.
+    (throw (ex-info "Missing one or more expressions, not yet implemented" {}))
+    ))
+
+(defn hyphenated-names
+  ;; Many French names are hyphenated such as "jean-albert" and "anne-marie".
+  ;; Given a name such as "jean" return all other names X such that "jean-X" or "X-jean"
+  ;; is a name registered in the resource data base.
+  ;; We only care about exact names.  for example "jean-jerome" and "jean-jérôme" are different.
+  ;; Do not attempt to elide names.
+  ;; Careful!  If the given baseName never appears as some hyphenated form,
+  ;;   then the empty Set should be returned.
+  ;; Careful!  Some names have multiple hyphens:  e.g., abd-el-kader
+  ;; Ignore any line for which year = XXXX.
+  ;; WARNING some name might be hyphenated with itself.  E.g. jean-jean or marie-marie.
+  ;;  Read the instructions carefully to understand what to do in this case.
+  [base-name]
+  (assert (= base-name (lower-case base-name))
+          (cl-format false "hyphenated-names must be called with lower case name, not [~A]" base-name))
+  (let [s (io/resource "France-baby-names/nat2020.csv")]
+    (with-open [r (io/reader s)]
+
+      ;; remember to drop the first line of file: sexe;preusuel;annais;nombre
+      ;; skip lines where name is _PRENOMS_RARES
+      ;; skip lines where the year is XXXX
+      ;; names are in caps (e.g., FABIENNE) but nameTarget will be given as lower case (e.g., fabienne)
+      ;; CHALLENGE: student must complete the implementation.
+      (throw (ex-info "Missing one or more expressions, not yet implemented" {}))
+      )))
---
+++ clojurein-source-code/test/clojurein_source_code/homework/frenchnames_test.clj
@@ -0,0 +1,174 @@
+(ns clojurein-source-code.homework.frenchnames-test
+  (:require [clojurein-source-code.homework.frenchnames :as sut]
+            [clojurein-source-code.common.util :refer [member]]
+            [clojure.pprint :refer [cl-format]]
+            [clojure.test :refer [deftest is testing]]))
+
+
+(deftest t-elide
+  (testing "elide"
+    (is (= "jerome"
+           (sut/elide-string "jèrome")))
+    (is (= "jerome"
+           (sut/elide-string "jèrôme")))
+    (is (= "come"
+           (sut/elide-string "côme")))))
+
+(deftest t-renomes-rare
+  (testing "prenoms rares"
+    (is (= {}
+           (sut/baby-names-per-year "_prenoms_rares" "M")))
+    (is (= {}
+           (sut/baby-names-per-year "_prenoms_rares" "F")))))
+
+(deftest t-baby-names-per-year
+  (testing "baby names per year"
+    (let [aaron (sut/baby-names-per-year "aaron" "M")]
+      (is (= (get aaron 2019) 2443))
+      (is (= (get aaron 2018) 2248))
+      (is (= (get aaron 2017) 2383))
+      (is (= (get aaron 2000) 118)))
+    (let [fabienne (sut/baby-names-per-year "fabienne""F")]
+      (is (= (fabienne 1999) 23))
+      (is (= (fabienne 1998) 32))
+      (is (= (fabienne 2010) 7))
+      (is (= (fabienne 2011) 7))
+      (is (= (fabienne 1962) 4739)))))
+
+(deftest t-count-names-0
+  (testing "count names in year range without elide"
+    (is (= (sut/count-names-in-year-range "aaron"  "M"  1900  2000  false)
+           723))))
+
+(deftest t-count-names-1
+  (testing "count names in year range without elide"
+    (is (= (sut/count-names-in-year-range "fabienne"  "M"  1900  2000  false)
+           0))
+    (is (= (sut/count-names-in-year-range "fabienne"  "F"  1900  2000  false)
+           105177))
+    (is (= (sut/count-names-in-year-range "fabienne"  "F"  1000  3000  false)
+           105326))
+    (is (= (sut/count-names-in-year-range "fabienne"  "F"  1962  1962  false)
+           4739))
+    (is (= (sut/count-names-in-year-range "jeremy"  "M"  1900  2000  false)
+           106063))
+    (is (= (sut/count-names-in-year-range "jerémy"  "M"  1900  2000  false)
+           99))
+    (is (= (sut/count-names-in-year-range "agnes"  "F"  1900  2050  false)
+           158))
+    (is (= (sut/count-names-in-year-range "agnés"  "F"  1900  2050  false)
+           0))
+    (is (= (sut/count-names-in-year-range "agnès"  "F"  1900  2050  false)
+           112773))))
+
+(deftest t-count-names-elide
+  (testing "count names in year range with elide"
+
+    (is (= (sut/count-names-in-year-range "jeremy"  "M"  1900  2000  true)
+           121220))
+    (is (= (sut/count-names-in-year-range "jerome"  "M"  1900  2000  true)
+           206081))
+    (is (= (sut/count-names-in-year-range "zumra"  "F"  2000  2050  true)
+           287))
+    (is (= (sut/count-names-in-year-range "agnes"  "F"  1900  2050  true)
+           112931))))
+
+(deftest t-count-names-2-elide
+  (testing "count names with elide"
+    ;; jerome vs jérôme vs jérome
+
+    (is (= {} (sut/baby-names-per-year "jérôme" "M"  true)))
+    (is (= {} (sut/baby-names-per-year "jerôme" "M"  true)))
+    (is (= {} (sut/baby-names-per-year "jérome" "M"  true)))
+    (is (= {} (sut/baby-names-per-year "jéröme" "M"  true)))
+
+
+    (let [j1 (sut/baby-names-per-year "jerome" "M"  false)
+          j2 (sut/baby-names-per-year "jérôme" "M"  false)
+          j3 (sut/baby-names-per-year "jérome" "M"  false)
+          j4 (sut/baby-names-per-year "jéröme" "M"  false)
+          j5 (sut/baby-names-per-year "jerôme" "M"  false)
+          j6 (sut/baby-names-per-year "jerome" "M"  true)]
+      (is (= (j1 1997) 525))
+      (is (= (j1 1956) 320))
+      (is (= (j1 1939) 73))
+      
+      (is (= (j2 1932) 3))
+      (is (= (j2 1947) 12))
+      (is (= (j2 1984) 763))
+      (is (= (j2 1979) 1174))
+
+      (is (= (j3 1981) 94))
+      (is (= (j3 1972) 87))
+      (is (= (j3 2013) 4))
+      (is (= (j3 1966) 29))
+      (is (= (j3 1998) 25))
+
+      (is (= (j4 1985) 4))
+      (is (= (j4 1975) 4))
+      (is (= (j4 1983) 3))
+      (is (= (j4 1974) 3))
+      (is (= (j4 1977) 7))
+
+      (is (= (j5 1968) 14))
+      (is (= (j5 1964) 13))
+      (is (= (j5 1965) 10))
+      (is (= (j5 1997) 11))
+
+      (is (not (= j6 j1)))
+
+      (doseq [year (range 1900 2031)]
+        (is (= (get j6 year 0)
+               (+ (get j1 year 0)
+                  (get j2 year 0)
+                  (get j3 year 0)
+                  (get j4 year 0)
+                  (get j5 year 0)))
+            (cl-format nil
+                       "year=~A ~A + ~A + ~A + ~A + ~A != ~A"
+                       year (get j1 year 0)
+                       (get j2 year 0)
+                       (get j3 year 0)
+                       (get j4 year 0)
+                       (get j5 year 0)
+                       (get j6 year 0)
+                       ))))))
+
+(deftest t-hyphenated-0a
+  (testing "hyphenated names 0a"
+    (is (not (member "jean" (sut/hyphenated-names "jean")))
+        "jean-jean only ppaears in a year XXXX which should be ignored")))
+
+(deftest t-hyphenated-0b
+  (testing "hyphenated names 0b"
+    (is (not (member "cyrille" (sut/hyphenated-names "cyrille"))))))
+
+(deftest t-hyphenated-0c
+  (testing "hyphenated names 0c"
+    (is (member "marie" (sut/hyphenated-names "marie")))))
+
+(deftest t-hyphenated-1
+  (testing "hyphenated names"
+
+
+    (let [jean  (sut/hyphenated-names "jean")]
+      (doseq [n ["mario"  "michael"  "cyrille"  "simon" 
+                 ;; "sully"  ;; only in year XXXX
+                 "cyril"  "yannick"  "lou"  "mael" 
+                 "lucien"  "eudes" 
+                 ;; "constant" ;; only in year XXXX
+                 ]]
+        (is (member n jean)))
+      (doseq [n ["sully"  "constant"]]
+        (is (not (member n jean))
+            (cl-format nil "failed to ignore year XXXX  thus found jean-~A" n))))
+    (is (= #{"sainte"  "anne"  "marie"}
+           (sut/hyphenated-names "catherine")))
+    (let [el  (sut/hyphenated-names "el")
+          marie (sut/hyphenated-names "marie")]
+      (is (= #{"habib"  "hadi"  "amine"  "kal"  "hadji" 
+               "krim"  "nour"  "rahman"  "hadj"  "houda"  "mehdi" 
+               "abd"  "kader"  "yamine"  "mohamed"}
+             el))
+      (doseq [name el]
+        (is (member "el" (sut/hyphenated-names name)))))))
