b0bbf5bdb6401e5bbd4c3d839e3744557ea9f34b
Jim Newton <jimka2001@users.noreply.github.com>
2024-06-24 11:19:58 +0200

    improving concurrency lecture

--- clojurein-source-code/src/repl_sessions/concurrency.clj
+++ clojurein-source-code/src/repl_sessions/concurrency.clj
@@ -8,9 +8,9 @@
             [clojure.tools.logging :as log]
             [com.climate.claypoole :as cp]))
 
-;; log/info is not working for me, so we use prn for the moment
 (defmacro pr-info [& body]
   `(log/info ~@body)
+  ;; convert back to prn to simplify the output
   ;; `(prn ~@body)
   )
 
@@ -31,35 +31,40 @@
     my-object))
 
 
-;; so why we even need to have parallel threads - to make your application faster
-;; and utilize resources better, if we have to spin 10 independent requests (and combine results later)
-;; taking each 5 seconds execute one-by-one is 50 seconds, but all in parallel is just 5sec (maybe a bit more)
+;; so why do we even need to have parallel threads? - to make your
+;; application faster and utilize resources better.  If we have to
+;; spin 10 independent requests (and combine results later) taking
+;; each 5 seconds execute one-by-one is 50 seconds, but all in
+;; parallel is just 5sec (maybe a bit more)
 
 
 ;; execute something in a thread
 
 (defn run-java-thread
-  []
+  [data]
   (pr-info "Before thread")
   (.start (new Thread (fn []
                         (Thread/sleep 1000)
-                        (pr-info "from thread"))))
+                        (pr-info (format "from thread: data=%s" data)))))
   (pr-info "After thread"))
 
 ;; but that's too much code + java interop
 
 ;; clojure Future instead
 (defn run-clojure-future
-  []
+  [data]
   (pr-info "Before future")
   (let [f
         (future
           (Thread/sleep 1000)
-          (pr-info "From future body")
-          (pr-info "from future")
-          {:result "from future"})]
+          (pr-info (format "From future body: data=%s" data))
+          (pr-info (format "from future body: data=%s" data))
+          {:result "from future"
+           :data data
+           })]
     (pr-info "After future")
-    (pr-info "Result of future is " (deref f)))
+    (pr-info "Result of future is " (deref f))
+    (pr-info "After deref"))
   )
 
 (defn serial-requests
@@ -72,6 +77,7 @@
 
     (->> urls
          (map (fn [url]
+                (pr-info (format "treating url: %s" url))
                 (-> (client/get url {:as :json})
                     :body
                     (select-keys [:name :shape]))))
@@ -88,11 +94,15 @@
 
     (->> urls
          (map (fn [url]
+                (pr-info (format "starting future for url: %s" url))
                 (future
                   (-> (client/get url {:as :json})
                       :body
                       (select-keys [:name :shape])))))
-         (map deref)
+         (doall)
+         (map (fn [f] 
+                (pr-info (format "deref %s" f))
+                (deref f)))
          (doall))))
 
 
@@ -102,19 +112,19 @@
 (def counter 0)
 
 (defn inc-counter-test
-  []
+  [n]
   (alter-var-root #'counter (constantly 0))
   (let [f1 (future
-             (dotimes [_ 1000]
+             (dotimes [_ n]
                ;(Thread/sleep 1)
                (alter-var-root #'counter (constantly (inc counter))))
              (pr-info "f1 ends")
              ::f1
              )
         f2 (future
-             (dotimes [_ 1000]
+             (dotimes [_ n]
                ;(Thread/sleep 2)
-               (alter-var-root #'counter (constantly (inc counter))))
+               (alter-var-root #'counter (constantly (dec counter))))
              (pr-info "f2 ends")
              ::f2)]
     (and (deref f1) (deref f2))
@@ -122,7 +132,6 @@
     counter
 
     )
-
   )
 
 (def counter-atom (atom 0))
@@ -130,22 +139,40 @@
 (def counter-atom-f2 (atom 0))
 
 (defn inc-counter-atom-test
-  []
+  "increment and decrement the atom the same number of times"
+  [n] ;; e.g. n=10000
+  (reset! counter-atom 0)
+
+  (let [f1 (future
+             (dotimes [_ n]
+               (swap! counter-atom inc))
+             ::f1)
+        f2 (future
+             (dotimes [_ n]
+               (swap! counter-atom dec))
+             ::f2)]
+    (pr-info "Here!" @f1 @f2)
+    (pr-info (deref counter-atom))
+))
+
+(defn inc-counter-atom-test
+  "increment and decrement the atom the same number of times, but count the re-tries"
+  [n] ;; e.g. n=10000
   (reset! counter-atom 0)
   (reset! counter-atom-f1 0)
   (reset! counter-atom-f2 0)
 
   (let [f1 (future
-             (dotimes [_ 10000]
+             (dotimes [_ n]
                (swap! counter-atom (fn [x]
                                      (swap! counter-atom-f1 inc)
                                      (inc x))))
              ::f1)
         f2 (future
-             (dotimes [_ 10000]
+             (dotimes [_ n]
                (swap! counter-atom (fn [x]
                                      (swap! counter-atom-f2 inc)
-                                     (inc x))))
+                                     (dec x))))
              ::f2)]
     (pr-info "Here!" @f1 @f2)
     (pr-info (deref counter-atom)
@@ -153,20 +180,25 @@
          (deref counter-atom-f2))
 ))
 
-(swap! counter-atom (fn [old-value arg1]
-                      (println old-value arg1)
-                      old-value
-                      ) 1)
+
+(comment
+  (swap! counter-atom (fn [old-value arg1]
+                        (println old-value arg1)
+                        old-value
+                        )
+         1))
 
 
 ;; be careful with pmap - it's potentially will not give you the boost as you expect
-;; so when you learn clojure there map but also pmap, and looks like people just assume that it's a magic fn to make
+;; so when you learn clojure there is map but also pmap,
+;; and looks like people just assume that it's a magic fn to make
 ;; the processing faster, it's actually try but only for cpu intensive tasks
 (comment (pmap (fn [input]) [1 2 3]))
 
 
 ;; futures are easy and great, but what if you want more control
-;; so futures are execute on an unbounded thread pool, so it's hard to control how much is running at max
+;; so futures are execute on an unbounded thread pool,
+;; so it's hard to control how much is running at max
 ;; we have claypoole library to the resque
 
 (defn claypoole-pmap-example
